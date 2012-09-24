e53db679be589a47c8e190d1a5ae81383b836cd7
zhouyrt <zhouyrt@163.com>
2012-09-24 17:54:34 +0800

    format code

--- e.js
+++ e.js
@@ -33,9 +33,7 @@ var // 每个element上绑定的一个唯一属性，递增
 	// 优先使用标准API
 	w3c = !!window.addEventListener,
 	
-	addListener, removeListener, trigger, util,
-	
-	toString = Object.prototype.toString
+	addListener, removeListener, trigger, util,	toString = Object.prototype.toString
 
 // utility functions -----------------------------------------------------------------------------
 util = {
@@ -123,7 +121,6 @@ util = {
 			return function(el, type, handler) { el.attachEvent('on' + type, handler) }
 		}
 	}()
-
 }
 
 // private functions ---------------------------------------------------------------------------
